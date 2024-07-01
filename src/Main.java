b286e07f4df35fa290cdccbd8c3bbf8430db58bb
Julia Bukova <ul4ik17@gmail.com>
2024-07-01 13:07:31 +0300

    commit1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int sum = 13676;
+        int b = 20;
+        int res = sum / b;
+        System.out.println("Стоимость билета: " + sum + " руб");
+        System.out.println("Кол-во начисленных миль: " + res + " миль");
+
+    }
+}
