02fdeb24852d3c7e26250c1b0dd0a7577b9ad4ac
Julia Bukova <ul4ik17@gmail.com>
2024-07-01 13:54:55 +0300

    commit2

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int sum = 13676;
-        int b = 20;
-        int res = sum / b;
-        System.out.println("Стоимость билета: " + sum + " руб");
-        System.out.println("Кол-во начисленных миль: " + res + " миль");
+        int price = 13676;
+        int bonus = 20;
+        int mile= price / bonus;
+        System.out.println("Стоимость билета: " + price + " руб");
+        System.out.println("Кол-во начисленных миль: " + mile + " миль");
 
     }
 }
