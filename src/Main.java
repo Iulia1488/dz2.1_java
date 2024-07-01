2e6e5af5f72aa3a710ca25e3561f69d27ee7e55a
Julia Bukova <ul4ik17@gmail.com>
2024-07-01 14:45:10 +0300

    commit3

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,8 @@ public class Main {
     public static void main(String[] args) {
         int price = 13676;
         int bonus = 20;
-        int mile= price / bonus;
+        int mile = price / bonus;
         System.out.println("Стоимость билета: " + price + " руб");
         System.out.println("Кол-во начисленных миль: " + mile + " миль");
-
     }
 }
